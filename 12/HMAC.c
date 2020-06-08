a31c9d682b701012519413cc40f05c5ac35a8922
Psyduck0409 <rdwentao@163.com>
2020-06-08 17:21:21 +0800

    HMAC debug

--- 12/HMAC.c
+++ 12/HMAC.c
@@ -15,7 +15,7 @@ void HMAC(unsigned char *hash,unsigned char *M,long long int mLen,unsigned char 
 int main(){
     unsigned char M[] = "Sample message for keylen=blocklen";
     long long int mLen = strlen(M);
-    unsigned char K[SHA1_BLOCK_SIZE] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63};
+    unsigned char K[64] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63};
 
     int b = 512;
 
@@ -39,8 +39,10 @@ void HMAC(unsigned char *hash,unsigned char *M,long long int mLen,unsigned char 
         free(zero);
         printf("paddKey\n" );
     }
-    else
-        K_ = K;
+    else{
+        K_ = (unsigned char*)malloc(sizeof(unsigned char )*(kLen));
+        memcpy(K_,K,kLen);
+    }
     unsigned char *S = (unsigned char*)malloc(sizeof(unsigned char) * bLen);
     for(int i =0; i< bLen; i++)
         S[i] = K_[i]^0x36;
@@ -52,12 +54,16 @@ void HMAC(unsigned char *hash,unsigned char *M,long long int mLen,unsigned char 
     sha1_update(&ctx, tmp, bLen+mLen);
     sha1_final(&ctx, hash);
 
+    for(int i =0;i<20;i++)
+        printf("%x ",hash[i]);
+    putchar('\n');
+
     unsigned char *S0 = (unsigned char*)malloc(sizeof(unsigned char) * bLen);
     for(int i =0; i< bLen; i++)
-        S[i] = K_[i]^0x5c;
+        S0[i] = K_[i]^0x5c;
 
     free(tmp);
-    tmp = array_append(hash, SHA1_BLOCK_SIZE, S0, bLen);
+    tmp = array_append( S0, bLen,hash,SHA1_BLOCK_SIZE);
 
     memset(hash,0,SHA1_BLOCK_SIZE);
     sha1_init(&ctx);
