1f3002295f9779aa25233942f7598cbeb9a226f7
Psyduck0409 <rdwentao@163.com>
2020-06-10 00:48:49 +0800

    sha1 update

--- 12/sha1.py
+++ 12/sha1.py
@@ -3,13 +3,17 @@
 
 def sha1(m:bytearray):
     mLen = len(m)
+    l = mLen * 8
     print(mLen)
+    m.append(0x80)
+    mLen +=1
     if mLen%64 != 56:
-        m.append(0x80)
-        for i in range(mLen%64 - 1):
+        while mLen %64!=56: 
             m.append(0)
-    m = m+ bytearray.fromhex("{:0>8x}".format(mLen))
+            mLen += 1
+    m = m+ bytearray.fromhex("{:0>16x}".format(l))
     mLen = len(m)
+    print(mLen)
 
     a = 0x67452301
     b = 0xEFCDAB89
@@ -19,6 +23,7 @@ def sha1(m:bytearray):
 
     for i in range(mLen//64):
         a,b,c,d,e = F(m[i*64:i*64+64],a,b,c,d,e) 
+
     return bytearray.fromhex("{:0>8x}".format(a)+"{:0>8x}".format(b)+"{:0>8x}".format(c)+"{:0>8x}".format(d)+"{:0>8x}".format(e))
 
 def F(m,aa,bb,cc,dd,ee):
@@ -31,34 +36,35 @@ def F(m,aa,bb,cc,dd,ee):
 
     w=[]
     for i in range(16):
-        w[i] = int(m[4*i:4*i+4].hex(),16)
+        w.append(int(m[4*i:4*i+4].hex(),16))
 
     for i in range(16,80):
         temp = w[i-3]^w[i-8]^w[i-14]^w[i-16]
-        w[i] = (temp<<1 | temp>>31)&0xffffffff
+        w.append((temp<<1 | temp>>31)&0xffffffff)
 
     for i in range(80):
-        temp2 = a<<5|a>>27
+        temp2 = (a<<5|a>>27)&0xffffffff
         if i//20 == 0:
             temp = (b&c)|((~(b))&d)
             temp += K[0]
         elif i//20 == 1:
             temp = (b^c^d)
-            temp += k[1]
+            temp += K[1]
         elif i//20 == 2:
             temp = (b&c)|(b&d)|(c&d)
-            temp += k[2]
+            temp += K[2]
         else:
             temp = (b^c^d)
-            temp += k[3]
-        temp += temp2 + e + w[j]
+            temp += K[3]
+        temp += temp2 + e + w[i]
+        temp &= 0xffffffff
         e = d;
         d = c;
         c = (b<<30 | b>>2)&0xffffffff; 
         b = a;
         a = temp;
-        return aa+a,bb+b,cc+c,dd+d,ee+e
+    return (aa+a)&0xffffffff,(bb+b)&0xffffffff,(cc+c)&0xffffffff,(dd+d)&0xffffffff,(ee+e)&0xffffffff
 
 if __name__ == "__main__":
-    m = bytearray.fromhex("8F7F6F5F4F3F2F1F0F")
+    m = bytearray("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",encoding='utf8')
     print(sha1(m).hex())
